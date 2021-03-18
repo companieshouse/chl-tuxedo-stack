eae467190dfab47a01cbb6854c260939a1532a7b
Marc Ransome <mransome@companieshouse.gov.uk>
2021-03-18 12:20:57 +0000

    Add SSM integration for SSH connectivity

--- groups/frontend/instance.tf
+++ groups/frontend/instance.tf
@@ -138,6 +138,7 @@ resource "aws_instance" "frontend" {
   placement_group = aws_placement_group.frontend.id
   subnet_id       = element(local.application_subnet_ids_by_az, count.index) # use 'element' function for wrap-around behaviour
 
+  iam_instance_profile   = module.instance_profile.aws_iam_instance_profile.name
   user_data_base64       = data.template_cloudinit_config.config[count.index].rendered
   vpc_security_group_ids = concat([aws_security_group.common.id], [for k, v in aws_security_group.services : v.id])
 
