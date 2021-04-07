68532f50d96f59ca2b63e77297f7479b7a0b4039
Marc Ransome <mransome@companieshouse.gov.uk>
2021-04-07 08:51:56 +0100

    Add support for individual user logs

--- groups/frontend/variables.tf
+++ groups/frontend/variables.tf
@@ -96,7 +96,7 @@ variable "service_subtype" {
 }
 
 
-variable "tuxedo_logs" {
+variable "tuxedo_service_logs" {
   type        = map(list(any))
   description = "A map whose keys represent server-side tuxedo server groups with lists of objects representing individual log files for each server group. Each object is expected to have at a minimum a 'name' key. Two CloudWatch log groups will be created for each object for standard output and standard error streams. Optional 'log_retention_in_days' and 'kms_key_id' attributes can be set per-oject to override the default values and will apply to both standard error and standard output log groups respectively."
   default = {
@@ -110,7 +110,6 @@ variable "tuxedo_logs" {
       { name: "VTNEP" },
       { name: "TRXML" }
     ]
-
     xml = [
       { name: "CHG" },
       { name: "BE" },
@@ -121,6 +120,19 @@ variable "tuxedo_logs" {
       { name: "VTNEP" },
       { name: "TRXML" },
       { name: "IXBRL" }
+    ]
+  }
+}
+
+variable "tuxedo_user_logs" {
+  type        = map(list(any))
+  description = "A map whose keys represent server-side tuxedo server groups with lists of objects representing individual log files for each server group. Each object is expected to have at a minimum a 'name' key. A single CloudWatch log group will be created for each object. Optional 'log_retention_in_days' and 'kms_key_id' attributes can be set per-oject to override the default values."
+  default = {
+    ewf = [
+      { name: "ULOG" }
+    ]
+    xml = [
+      { name: "ULOG" }
     ]
   }
 }
