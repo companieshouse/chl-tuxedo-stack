0b4f20deb6c0134c38ba707d3b29b5e7e7286afe
Marc Ransome <mransome@companieshouse.gov.uk>
2021-04-30 08:25:35 +0100

    Construct Oracle Database config at instance startup

--- groups/frontend/secrets.tf
+++ groups/frontend/secrets.tf
@@ -9,3 +9,7 @@ data "vault_generic_secret" "security_s3_buckets" {
 data "vault_generic_secret" "security_kms_keys" {
   path = "aws-accounts/security/kms"
 }
+
+data "vault_generic_secret" "tns_names" {
+  path = "applications/${var.aws_account}-${var.region}/tuxedo/tnsnames"
+}
