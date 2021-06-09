deb4a9eec93c3b16773bbc847ab128efed0a5f5d
Marc Ransome <mransome@companieshouse.gov.uk>
2021-06-09 08:32:33 +0100

    Fix data structure types

--- groups/frontend/instance.tf
+++ groups/frontend/instance.tf
@@ -87,14 +87,14 @@ resource "aws_security_group" "services" {
 
   # TODO Remove this; this was added for testing Tuxedo services in live using on-premise frontend services
   dynamic "ingress" {
-    for_each = var.environment == "live" ? each.value : []
+    for_each = var.environment == "live" ? each.value : {}
     iterator = service
     content {
       description = "Allow client requests from on-premise frontend web servers to ${service.key} service in ${each.key} server group"
       from_port   = service.value
       to_port     = service.value
       protocol    = "TCP"
-      cidr_blocks = var.on_premise_frontend_cidr
+      cidr_blocks = [var.on_premise_frontend_cidr]
     }
   }
 
