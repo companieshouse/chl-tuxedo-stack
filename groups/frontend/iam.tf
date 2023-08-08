6e03a9843f18e2cdd29afc30c40e0d05325fd135
Marc Ransome <mransome@companieshouse.gov.uk>
2023-08-08 08:39:23 +0100

    Update AWS provider and external modules

--- groups/frontend/iam.tf
+++ groups/frontend/iam.tf
@@ -1,9 +1,9 @@
 module "instance_profile" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/instance_profile?ref=tags/1.0.62"
+  source = "git@github.com:companieshouse/terraform-modules//aws/instance_profile?ref=tags/1.0.189"
   name   = "tuxedo-frontend-profile"
 
   cw_log_group_arns = flatten([
-    [aws_cloudwatch_log_group.cloudwatch.arn],
+    ["${aws_cloudwatch_log_group.cloudwatch.arn}:*"],
     [for tuxedo_server_type_key, tuxedo_services in var.tuxedo_services : "arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:log-group:${var.service_subtype}-${var.service}-${tuxedo_server_type_key}-*:*"]
   ])
 
